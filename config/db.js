1485c1cd8fac15f80804c55c967b37795bd3a648
UmairFarooqLone <umairibnfarooq1@gmail.com>
2024-09-12 13:48:05 +0530

    revert config file

--- config/db.js
+++ config/db.js
@@ -3,23 +3,10 @@ require('dotenv').config();
 
 async function connect() {
   try {
-    // Check if MONGO_URI is properly set in the environment variables
-    const mongoURI = process.env.MONGO_URI;
-    
-    if (!mongoURI) {
-      throw new Error('MONGO_URI is not defined in your .env file.');
-    }
-
-    // Connect to MongoDB using the provided URI
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB!");
   } catch (err) {
-    console.error("Error connecting to MongoDB: ", err.message);
-    process.exit(1); // Exit the process if the connection fails
+    console.error("Error connecting to MongoDB: ", err);
   }
 }
 
